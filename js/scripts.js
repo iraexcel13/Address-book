9c9895a55c632be125dadfba1274b2d7923cbdae
Iroveena Pagsibigan and Sharon Gray <iroveena@>
2017-06-28 19:11:11 -0700

    Add address section to the webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,70 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 }
 function Address(street, city, state) {
-  this.streetName = street;
-  this.cityName = city;
-  this.stateName = state;
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 Address.prototype.fullAddress = function() {
-  return this.streetName + ", " + this.cityName + ", " + this.stateName;
+  return this.street + ", " + this.city + " " + this.state;
 }
 
 $(document).ready(function() {
-  $("#button").click(function() {
-    $(".output").show();
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+    '<div class="form-group">' +
+    '<label for="new-street">Street</label>' +
+    '<input type="text" class="form-control new-street">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-city">City</label>' +
+    '<input type="text" class="form-control new-city">' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-state">State</label>' +
+    '<input type="text" class="form-control new-state">' +
+    '</div>' +
+    '</div>');
   });
-  $("#button2").click(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    // var inputtedStreet = $("input#new-street").val();
+    // var inputtedCity = $("input#new-city").val();
+    // var inputtedState = $("input#new-state").val();
 
-  var inputtedFirstName = $("input#new-first-name").val();
-  var inputtedLastName = $("input#new-last-name").val();
-  var inputtedStreet = $("input#new-street").val();
-  var inputtedCity = $("input#new-city").val();
-  var inputtedState = $("input#new-state").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-  var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find('input.new-street').val();
+      var inputtedCity = $(this).find('input.new-city').val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      newContact.addresses.push(newAddress);
+    })
 
-$(".contact").last().click(function() {
-  $("#show-contact").show();
-  $("#show-contact h2").text(newContact.fullName());
-  $(".first-name").text(newContact.firstName);
-  $(".last-name").text(newContact.lastName);
-  $(".address-name").text(newAddress.fullAddress())
-});
-  // $("input#new-first-name").val("");
-  // $("input#new-last-name").val("");
-});
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
+    // $("input#new-first-name").val("");
+    //   // $("input#new-last-name").val("");
+  });
 });
